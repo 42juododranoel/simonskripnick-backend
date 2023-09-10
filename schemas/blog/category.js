ffb0770b04ea747d2ab9a0ccdc23459c249128f2
Vsevolod Skripnik <vskripnik@bumerang.com>
2023-09-10 23:38:41 +0400

    add category backgrounds

--- schemas/blog/category.js
+++ schemas/blog/category.js
@@ -35,5 +35,10 @@ export default {
       title: 'Background',
       type: 'image',
     },
+    {
+      name: 'backgroundColor',
+      title: 'Background Color',
+      type: 'string',
+    },
   ],
 }
